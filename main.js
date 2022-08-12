42c0c1ad4b77bf6ac648fdf8c90e7f068e48c25e
zawho <zthooper2@gmail.com>
2022-08-12 11:58:28 +0800

    Add basic game loop function and update variables

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 //Global variables.
-let userSelection = prompt('Rock, paper, or scissors?', '');
-let computerSelection = getComputerChoice();
-let winVar;
-let loseVar;
-let tieVar;
-let invalidVar;
+let userScore = 0;
+let computerScore = 0;
 
 //PC takes a turn by randomly selecting rock, paper, or scissors. Return selection.
 function getComputerChoice() {
@@ -16,26 +12,29 @@ function getComputerChoice() {
 //User inputs their choice, user input and PC choice compared, round winner declared.
 function playRound(userSelection, computerSelection) {
     if ((userSelection.toLowerCase() === 'rock') && (computerSelection === 'scissors')) {
-        return winVar;
+        return userScore++;
     } else if ((userSelection.toLowerCase() === 'paper') && (computerSelection === 'rock')) {
-        return winVar;
+        return userScore++;
     } else if ((userSelection.toLowerCase() === 'scissors') && (computerSelection === 'paper')) {
-        return winVar;
+        return userScore++;
     } else if (userSelection.toLowerCase() === computerSelection) {
-        return tieVar;
+        return 'Tie!';
     } else if (!(userSelection.toLowerCase() === 'rock') && !(userSelection.toLowerCase() === 'paper') && !(userSelection.toLowerCase() === 'scissors')) {
-        return invalidVar;
+        return 'Invalid!';
     } else {
-        return loseVar;
+        return computerScore++;
     }
 }
 
 //Loop single round 5 times for a full game. Declare winner.
 function playGame() {
-    playRound(userSelection, computerSelection);
-        }
+    for (i = 0; i < 5; i++) {
+        let userSelection = prompt('Rock, paper, or scissors?', '');
+        let computerSelection = getComputerChoice();
+        playRound(userSelection, computerSelection);
+        console.log(i);
+    }
+}
 
 //Console output.
-console.log('Computer chose: ' + computerSelection);
-console.log('You chose: ' + userSelection);
 console.log(playGame());
