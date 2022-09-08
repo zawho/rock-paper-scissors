6df8b92b546c8ec99db3e2502f83641314ba70ec
zawho <zthooper2@gmail.com>
2022-09-08 09:59:39 +0800

    Add score counter and end game function

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const rockButton = document.querySelector('.rock');
 const paperButton = document.querySelector('.paper');
 const scissorsButton = document.querySelector('.scissors');
 const roundResultsDiv = document.querySelector('.round-results');
+const roundScoreDiv = document.querySelector('.round-score');
 const gameResultsDiv = document.querySelector('.game-results');
 
 //PC takes a turn by randomly selecting rock, paper, or scissors. Return selection.
@@ -20,7 +21,7 @@ function getComputerChoice() {
 //User inputs their choice, user input and PC choice compared, round winner declared.
 function playRound(userSelection) {
     let computerSelection = getComputerChoice();
-    let roundMessage = `You chose ${userSelection} and computer chose ${computerSelection}! \nYOU: ${userScore} COMPUTER: ${computerScore}`;
+    let roundMessage = `You chose ${userSelection} and computer chose ${computerSelection}!`;
     if (userSelection === computerSelection) {
         roundResultsDiv.innerText = `Tie! ${roundMessage}`;
     } else if ((userSelection === 'rock') && (computerSelection === 'scissors')) {
@@ -42,21 +43,25 @@ function endGame() {
     if (userScore === 5 || computerScore === 5) {
         getWinner();
     }
-    return;
 }
 
 //Loop single round 5 times for a full game.
 function playGame() {
     rockButton.addEventListener('click', function() {
         playRound('rock');
+        roundScoreDiv.innerText = `YOU: ${userScore} COMPUTER: ${computerScore}`;
+        endGame();
     });
     paperButton.addEventListener('click', function() {
         playRound('paper');
+        roundScoreDiv.innerText = `YOU: ${userScore} COMPUTER: ${computerScore}`;
+        endGame();
     });
     scissorsButton.addEventListener('click', function() {
         playRound('scissors');
+        roundScoreDiv.innerText = `YOU: ${userScore} COMPUTER: ${computerScore}`;
+        endGame();
     });
-    endGame();
 }
 
 //Declare winner.
