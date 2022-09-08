1dd88ebedd1ebda807d48387f896881ea146a42b
zawho <zthooper2@gmail.com>
2022-09-08 09:21:40 +0800

    Edit results divs and move event listeners

--- main.js
+++ main.js
@@ -7,16 +7,8 @@ let userSelection;
 const rockButton = document.querySelector('.rock');
 const paperButton = document.querySelector('.paper');
 const scissorsButton = document.querySelector('.scissors');
-const resultsDiv = document.querySelector('.results');
-rockButton.addEventListener('click', function() {
-    playRound('rock');
-});
-paperButton.addEventListener('click', function() {
-    playRound('paper');
-});
-scissorsButton.addEventListener('click', function() {
-    playRound('scissors');
-});
+const roundResultsDiv = document.querySelector('.round-results');
+const gameResultsDiv = document.querySelector('.game-results');
 
 //PC takes a turn by randomly selecting rock, paper, or scissors. Return selection.
 function getComputerChoice() {
@@ -30,41 +22,53 @@ function playRound(userSelection) {
     let computerSelection = getComputerChoice();
     let roundMessage = `You chose ${userSelection} and computer chose ${computerSelection}! \nYOU: ${userScore} COMPUTER: ${computerScore}`;
     if (userSelection === computerSelection) {
-        resultsDiv.innerText = `Tie! ${roundMessage}`;
+        roundResultsDiv.innerText = `Tie! ${roundMessage}`;
     } else if ((userSelection === 'rock') && (computerSelection === 'scissors')) {
         userScore += 1;
-        resultsDiv.innerText = roundMessage;
+        roundResultsDiv.innerText = roundMessage;
     } else if ((userSelection === 'paper') && (computerSelection === 'rock')) {
         userScore += 1;
-        resultsDiv.innerText = roundMessage;
+        roundResultsDiv.innerText = roundMessage;
     } else if ((userSelection === 'scissors') && (computerSelection === 'paper')) {
         userScore += 1;
-        resultsDiv.innerText = roundMessage; 
+        roundResultsDiv.innerText = roundMessage; 
     } else {
         computerScore += 1;
-        resultsDiv.innerText = roundMessage;
+        roundResultsDiv.innerText = roundMessage;
     }
+}
+
+function endGame() {
+    if (userScore === 5 || computerScore === 5) {
+        getWinner();
+    }
+    return;
 }
 
 //Loop single round 5 times for a full game.
 function playGame() {
-//    for (i = 0; i < 5; i++) {
-        console.log(playRound());
-        console.log(`YOU: ${userScore} COMPUTER: ${computerScore}`);
-//    }
-    getWinner();
+    rockButton.addEventListener('click', function() {
+        playRound('rock');
+    });
+    paperButton.addEventListener('click', function() {
+        playRound('paper');
+    });
+    scissorsButton.addEventListener('click', function() {
+        playRound('scissors');
+    });
+    endGame();
 }
 
 //Declare winner.
 function getWinner() {
     if (userScore > computerScore) {
-        console.log('You win!');
+        gameResultsDiv.innerText = 'You win!';
     } else if (userScore < computerScore) {
-        console.log('You lose! Computer wins!');
+        gameResultsDiv.innerText = 'You lose! Computer wins!';
     } else {
-        console.log('A tie! Please try again!');
+        gameResultsDiv.innerText = 'A tie! Please try again!';
     }
 }
 
 //Call game function.
-//playRound();
+playGame();
